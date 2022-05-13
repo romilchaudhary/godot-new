bbb954a3fa455da76431fc3abb32135524688dc6
romilchaudhary <romil.roar@gmail.com>
2022-05-13 17:19:11 +0530

    secret key

--- godot/godot/settings.py
+++ godot/godot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qar_c^d+n%tv0(pl=_8q91au(t+n*5)-&0zpfs*s7wu_(yci9+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
